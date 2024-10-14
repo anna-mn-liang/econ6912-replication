e0c5e70131d179accaedfd84d8c2c71ccfc02ad3
anna-mn-liang <anna.mn.liang@outlook.com>
2024-10-15 06:45:30 +1100

    Feat: Generating Fig 2
    Scripts and data to replicate Fig 2. Needs fixing to get conf intervals working.

--- src/scripts/functions.py
+++ src/scripts/functions.py
@@ -1,8 +1,7 @@
 import numpy as np 
 import pandas as pd
 
-from scripts.classes import ModelStruct
-
+from scripts.classes import VARStruct, ModelStruct, VARciStruct
 
 # function to create a lagged dataframe 
 def lagmatrix(df, lags):
@@ -68,3 +67,130 @@ def doProxySVAR(VAR):
 
 
     return modelVAR
+
+
+def doProxySVARci(VAR, modelVAR, clevel, nboot, BlockSize):
+    nBlock = int(np.ceil(modelVAR.Tval/BlockSize))
+
+    # Create blocks and centerings
+    Blocks = np.zeros((BlockSize, modelVAR.n, modelVAR.Tval - BlockSize + 1))
+    MBlocks = np.zeros((BlockSize, modelVAR.k, modelVAR.Tval - BlockSize + 1))
+
+    for j in range(modelVAR.Tval - BlockSize + 1):
+        Blocks[:, :, j] = modelVAR.res.iloc[j:j+BlockSize, :]
+        MBlocks[:, :, j] = modelVAR.m.iloc[j:j+BlockSize, :]
+
+    # Center the bootstrapped VAR errors
+    centering = np.zeros((BlockSize, modelVAR.n))
+    for j in range(BlockSize):
+        centering[j, :] = modelVAR.res.iloc[j:modelVAR.Tval - BlockSize + j + 1, :].mean(axis=0)
+
+    centering = np.tile(centering, (nBlock, 1))
+    centering = centering[:modelVAR.Tval, :]
+
+    # Center the boostrapped proxy variables
+    Mcentering = np.zeros((BlockSize, modelVAR.k))
+
+    # Only include the modelVAR.k=1 case. The original code includes the k=2 case. 
+    for j in range(BlockSize):
+        subM = np.asarray(modelVAR.m.iloc[j:modelVAR.Tval - BlockSize + j + 1, :])
+        subM2 = np.asarray(VAR.proxies)
+
+        # Filter rows where the first column is not zero
+        subM_filtered = subM[subM[:, 0] != 0]
+        subM2_filtered = subM2[subM2[:, 0] != 0]
+
+        # Compute the means and assign to Mcentering
+        Mcentering[j, :] = subM_filtered[:, 0].mean() - subM2_filtered[:, 0].mean()
+
+    Mcentering = np.tile(Mcentering, (nBlock, 1))
+    Mcentering = Mcentering[:modelVAR.Tval, :]
+
+    jj = 1
+
+    IRS = np.zeros((modelVAR.irs.shape[0]*modelVAR.irs.shape[1], nboot))
+
+    while jj < nboot + 1:
+        # draw bootstrapped residuals and proxies
+        # generate random indices
+        index = (np.ceil(modelVAR.Tval - BlockSize + 1) * np.random.rand(nBlock, 1)).astype(int)
+
+        U_boot = np.zeros((nBlock * BlockSize, modelVAR.n))
+        M_boot = np.zeros((nBlock * BlockSize, modelVAR.k))
+
+        for j in range(nBlock):
+            U_boot[BlockSize * j:BlockSize * (j + 1), :] = Blocks[:, :, index[j, 0] - 1]
+            M_boot[BlockSize * j:BlockSize * (j + 1), :] = MBlocks[:, :, index[j, 0] - 1]
+        
+        U_boot = U_boot[:modelVAR.Tval, :]
+        M_boot = M_boot[:modelVAR.Tval, :]
+
+        # Center the bootstrapped residuals and proxies
+        U_boot = U_boot - centering
+
+        for j in range(modelVAR.k):
+            non_zero_indices = M_boot[:, j] != 0
+            M_boot[non_zero_indices, j] = M_boot[non_zero_indices, j] - Mcentering[non_zero_indices, j]
+
+        resb = U_boot.T
+
+        varsb = np.zeros((VAR.p + modelVAR.Tval, modelVAR.n))
+        varsb[:VAR.p, :] = VAR.vars.iloc[:VAR.p, :]
+
+        for j in range(VAR.p, VAR.p + modelVAR.Tval):
+            lvars = varsb[j-1:(None if j==VAR.p else (j-VAR.p-1)):-1, :].T
+            varsb[j, :] = (lvars.T.flatten() @ modelVAR.bet[:VAR.p * modelVAR.n, :]) \
+                    + (modelVAR.det[j, :] @ modelVAR.bet[VAR.p * modelVAR.n:, :]) + resb[:, j - VAR.p]
+
+        proxies_df = pd.DataFrame(np.vstack((VAR.proxies.iloc[:VAR.p, :], M_boot)))
+        proxies_df.columns = VAR.proxies.columns
+
+        varsb_df = pd.DataFrame(varsb)
+        varsb_df.columns = VAR.vars.columns
+
+
+        VARBS = VARStruct(p=VAR.p, irhor=VAR.irhor, vars=varsb_df, proxies=proxies_df)
+
+        modelVARBS = doProxySVAR(VARBS)
+
+        # for i in range(modelVARBS.irs.shape[2]):
+        irs = np.asarray(modelVARBS.irs.iloc[:, :])
+        IRS[:, jj-1] = irs.T.flatten()
+
+        jj += 1
+
+
+    # Initialize irsH, irsL, and irs with zeros
+    irsHBS = np.zeros((VAR.irhor, modelVAR.irs.shape[1]))
+    irsLBS = np.zeros((VAR.irhor, modelVAR.irs.shape[1]))
+    irsBS = np.zeros((VAR.irhor, modelVAR.irs.shape[1]))
+
+    # Initialize irsHhall and irsLhall with zeros
+    irsHhallBS = np.zeros((VAR.irhor, modelVAR.irs.shape[1]))
+    irsLhallBS = np.zeros((VAR.irhor, modelVAR.irs.shape[1]))
+
+    # Confidence Bands
+    ##################
+    irsHBS[:, :] = np.quantile(IRS[:, :].T, (1 - clevel / 100) / 2, axis=0).reshape(modelVAR.irs.shape[1], VAR.irhor).T
+    irsLBS[:, :] = np.quantile(IRS[:, :].T, 1 - (1 - clevel / 100) / 2, axis=0).reshape(modelVAR.irs.shape[1], VAR.irhor).T
+    irsBS[:, :] = np.quantile(IRS[:, :].T, 1 - (1 - 50 / 100), axis=0).reshape(modelVAR.irs.shape[1], VAR.irhor).T
+
+    # virs = np.asarray(modelVAR.irs.iloc[:, :])
+    # irsHhallBS[:, :] = modelVAR.irs.iloc[:, :] - np.quantile((IRS[ :, :].T - np.tile(virs.T.flatten(), (nboot, 1))), (1 - clevel / 100) / 2, axis=0).reshape(VAR.irhor, modelVAR.irs.shape[1])
+    # irsLhallBS[:, :] = modelVAR.irs.iloc[:, :] - np.quantile((IRS[:, :].T - np.tile(virs.T.flatten(), (nboot, 1))), 1 - (1 - clevel / 100) / 2, axis=0).reshape(VAR.irhor, modelVAR.irs.shape[1])
+
+    irsHBS_df = pd.DataFrame(irsHBS)
+    irsLBS_df = pd.DataFrame(irsLBS)
+    irsBS_df = pd.DataFrame(irsBS)
+    irsHhallBS_df = pd.DataFrame(irsHhallBS)
+    irsLhallBS_df = pd.DataFrame(irsLhallBS)
+
+    irsHBS_df.columns = modelVAR.irs.columns
+    irsLBS_df.columns = modelVAR.irs.columns
+    irsBS_df.columns = modelVAR.irs.columns
+    irsHhallBS_df.columns = modelVAR.irs.columns
+    irsLhallBS_df.columns = modelVAR.irs.columns
+
+    VARci = VARciStruct(irsH=irsHBS_df, irsL=irsLBS_df, irs=irsBS_df, irsHhall=irsHhallBS_df, irsLhall=irsLhallBS_df)
+
+    return VARci
---
+++ src/scripts/replicate_fig2.py
@@ -0,0 +1,131 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from econ_lib import data_dir
+print(data_dir)
+
+import numpy as np 
+import pandas as pd
+
+# %%
+mdata_filepath = data_dir / "quarterly_hfi_var_data.xlsx"
+mdata = pd.read_excel(mdata_filepath)
+
+# Apply transformations to full dataset
+# Scale CPI, IP, rents, CPInet, PCE, PCEnet
+mdata.loc[:, ["CPI", "IP"]] = np.log(mdata.loc[:,["CPI", "IP"]])*100
+mdata.loc[:, ["HP", "HR"]] = np.log(mdata.loc[:,["HP", "HR"]])*100
+mdata.loc[:, ["HP", "HR"]] = np.log(mdata.loc[:,["HP", "HR"]])*100 # 2nd log as per matlab code
+mdata.loc[:, ["realhp", "realhr", "owneq"]] = np.log(mdata.loc[:,["realhp", "realhr", "owneq"]])*100
+mdata = mdata.fillna(0)
+mdata = mdata.query("year >=1981")
+mdata
+
+# %%
+pdata_filepath = data_dir / "quarterly_factors.xlsx"
+pdata = pd.read_excel(pdata_filepath)
+pdata
+
+# %%
+ppdata = mdata[['year', 'quarter']]
+ppdata = ppdata.merge(pdata, on=['year', 'quarter'], how='left')
+ppdata = ppdata.fillna(0)
+ppdata
+
+# %%
+from dataclasses import dataclass
+from scripts.classes import VARStruct, ModelStruct, VARciStruct
+import sys
+sys.path.append('/Users/annaliang/Documents/Repositories/econ6912-replication/src/scripts')
+from functions import doProxySVAR, doProxySVARci
+
+# varlist = ['HP', 'HR', 'RVR', 'HOMEOWN']
+varlist = ['HP']
+
+# Parameters
+nboot = 5000; # Number of Bootstrap Samples (Paper does 5000)
+clevel = 68; # Bootstrap Percentile Shown
+BlockSize = int(np.floor(5.03*len(mdata)**0.25)); # size of blocks in the MBB bootstrap
+seed = 2; # seed for random number generator
+np.random.seed(seed)
+p=4
+irhor=20
+
+irs_plot = np.zeros((irhor, len(varlist)))
+irsH_plot = np.zeros((irhor, len(varlist)))
+irsL_plot = np.zeros((irhor, len(varlist)))
+
+for x_var in varlist:
+    # Define VAR struct with hardcoded variables
+    VAR = VARStruct(p, irhor, vars=mdata[['gs1', 'IP', 'ebp', 'CPI', x_var]], proxies=ppdata[['ff4_tc']])
+
+    modelVAR = doProxySVAR(VAR)
+    modelVAR.irs
+
+    VARci = doProxySVARci(VAR, modelVAR, clevel, nboot, BlockSize)
+
+    shocksize = -0.25
+    shock = 1
+
+    modelVAR.irs = modelVAR.irs * shocksize
+    VARci.irsH = VARci.irsH * shocksize
+    VARci.irsL = VARci.irsL * shocksize
+    VARci.irs = VARci.irs * shocksize
+
+    # irs_plot[:, varlist.index(x_var)] = VARci.irs.iloc[:, 4]
+    # irsH_plot[:, varlist.index(x_var)] = VARci.irsH.iloc[:, 4]
+    # irsL_plot[:, varlist.index(x_var)] = VARci.irsL.iloc[:, 4]
+
+
+# irs_plot = pd.DataFrame(irs_plot, columns=varlist)
+# irsH_plot = pd.DataFrame(irs_plot, columns=varlist)
+# irsL_plot = pd.DataFrame(irs_plot, columns=varlist)
+
+# VARci = VARciStruct(irsH=irsH_plot, irsL=irsL_plot, irs=irs_plot, irsHhall=irsH_plot, irsLhall=irsL_plot)
+
+# %%
+import matplotlib.pyplot as plt
+
+plotdisplay = ['HP', 'HR', 'RVR', 'HOMEOWN']
+FigLabels = ['Housing Prices', 'Housing Rents', 'Housing Stock for Renting Vacancy Rate', 'Homeownership Rate']
+
+# display1 = np.array([0, 1, 2, 3])
+display1 = np.array([4])
+
+fig, axes = plt.subplots(2, 2, figsize=(10,8), constrained_layout=True)
+axes = axes.flatten()
+
+for nvar in range(len(display1)):
+    if VARci is not None:
+        ax = axes[nvar]
+        h, = ax.plot(range(VAR.irhor), VARci.irs.iloc[:, display1[nvar]], 'r', linewidth=1.5)
+        
+        ax.set_xlim([0, VAR.irhor - 1])
+        
+        if VARci.irsH is not None:
+            ax.fill_between(range(VAR.irhor), VARci.irsH.iloc[:, display1[nvar]], VARci.irsL.iloc[:, display1[nvar]], color='blue', alpha=0.15)
+        
+        ax.axhline(0, color='k', linestyle='-')
+        title_text = FigLabels[nvar]
+        ax.set_title(title_text, fontsize=12)
+        ax.set_xticks(np.arange(0, VAR.irhor, 6))
+        ax.grid(True)
+        ax.xaxis.grid(False)
+        ax.yaxis.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+# %%
