4724972b4fae56a19402cc341b89dc5b218a56de
anna-mn-liang <anna.mn.liang@outlook.com>
2024-10-20 19:54:08 +1100

    Feat: add outputs dir and move data dir

--- src/econ_lib/__init__.py
+++ src/econ_lib/__init__.py
@@ -1,4 +1,6 @@
 from pathlib import Path
 __version__ = "0.0.1"
 
-data_dir = Path(__file__).parent.parent / "data"
+data_dir = Path(__file__).parent.parent.parent / "data"
+outputs_dir = Path(__file__).parent.parent.parent / "outputs"
+outputs_dir.mkdir(exist_ok=True, parents=True)
